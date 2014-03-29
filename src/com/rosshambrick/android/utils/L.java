86410518269e296d086b8650251d97384f294b59
Ross Hambrick <ross@bignerdranch.com>
2014-03-29 19:59:29 -0400

    Factored out a getName() method that handles caching the TAG values

--- src/com/rosshambrick/android/utils/L.java
+++ src/com/rosshambrick/android/utils/L.java
@@ -1,5 +1,6 @@
 package com.rosshambrick.android.utils;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
 import android.os.Handler;
@@ -7,14 +8,19 @@ import android.os.Looper;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class L {
+    private static Map<Object, String> sMap = new HashMap<Object, String>();
+
     //DEBUG
     public static void d(Object object, String message, Throwable e) {
-        d(object.getClass().getName(), message, e);
+        d(getName(object), message, e);
     }
 
     public static void d(Object object, String message) {
-        d(object.getClass().getName(), message);
+        d(getName(object), message);
     }
 
     public static void d(String tag, String message, Throwable e) {
@@ -31,12 +37,12 @@ public class L {
 
     //ERROR
     public static void e(Object object, String message, Throwable e) {
-        Log.e(object.getClass().getName(), message, e);
+        Log.e(getName(object), message, e);
         toastIfDebug(object, message);
     }
 
     public static void e(Object object, String message) {
-        Log.e(object.getClass().getName(), message);
+        Log.e(getName(object), message);
         toastIfDebug(object, message);
     }
 
@@ -44,6 +50,7 @@ public class L {
         e(object, e.getMessage(), e);
     }
 
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     private static void toastIfDebug(Object object, String message) {
         if (BuildConfig.DEBUG) {
             if (object instanceof Context) {
@@ -75,20 +82,30 @@ public class L {
 
     //INFO
     public static void i(Object object, String message) {
-        Log.i(object.getClass().getName(), message);
+        Log.i(getName(object), message);
     }
 
     public static void i(Object object, String message, Throwable e) {
-        Log.i(object.getClass().getName(), message, e);
+        Log.i(getName(object), message, e);
     }
 
     //WARN
     public static void w(Object object, String message) {
-        Log.w(object.getClass().getName(), message);
+        Log.w(getName(object), message);
     }
 
     public static void w(Object object, String message, Throwable e) {
-        Log.w(object.getClass().getName(), message, e);
+        Log.w(getName(object), message, e);
+    }
+
+    //private
+    private static String getName(Object object) {
+        String name = sMap.get(object);
+        if (name == null) {
+            name = object.getClass().getName();
+            sMap.put(object, name);
+        }
+        return name;
     }
 
 }
